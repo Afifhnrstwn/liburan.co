b14b3f18a0ab69c77e699878337e086174a666f0
Afifhnrstwn <afifhnristwn@gmail.com>
2024-09-12 09:10:43 +0700

    commit awal

---
+++ main.js
@@ -0,0 +1,6 @@
+// const img = document.querySelector(".img").cloneNode(true);
+// document.querySelector(".img-container").appendChild(img);
+
+
+const logo = document.querySelector(".logo").cloneNode(true);
+document.querySelector(".logo-container").appendChild(logo);
